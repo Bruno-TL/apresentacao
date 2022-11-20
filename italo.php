9919b2b4168cfc89c844e664f7a05e9742eb0caf
ItaloLevi0 <italoleviti@gmail.com>
2022-11-20 09:19:02 -0300

    exemplo shuffle

---
+++ italo.php
@@ -0,0 +1,28 @@
+<?php
+// Exemplo com array associativo
+
+$animais = [
+    "a" => "Cachorro",
+    "b" => "Gato",
+    "c" => "Papagaio",
+];
+
+shuffle($animais);
+
+print_r($animais);
+
+
+//Exemplo com array não associativo
+
+$biscoitos = [
+    "Negresco",
+    "Passatempo",
+    "Oreo",
+];
+
+shuffle($biscoitos);
+
+print_r($biscoitos);
+
+
+
